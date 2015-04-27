91ea132c01a68daa49c5bf94a515e17501effa4c
Joe Ibershoff <joe@ibershoff.com>
2015-04-27 15:33:14 -0500

    refactored for Promises

---
+++ lib/client.coffee
@@ -0,0 +1,102 @@
+
+crypto = require('crypto')
+request = require('request')
+Promise = require('bluebird')
+
+auth = require('./auth.js')
+metadata = require('./metadata.js')
+search = require('./search.js')
+object = require('./object.js')
+appUtils = require('./utils.js')
+
+URL_KEYS =
+  GET_METADATA: "GetMetadata"
+  GET_OBJECT: "GetObject"
+  SEARCH: "Search"
+  UPDATE: "Update"
+  ACTION: "Action"
+  LOGIN: "Login"
+  LOGOUT: "Logout"
+  
+OTHER_KEYS =
+  MEMBER_NAME: "MemberName"
+  USER: "User"
+  BROKER: "Broker"
+  METADATA_VERSION: "MetadataVersion"
+  METADATA_TIMESTAMP: "MetadataTimestamp"
+  MIN_METADATA_TIMESTAMP: "MinMetadataTimestamp"
+  RETS_VERSION: "retsVersion"
+  RETS_SERVER: "retsServer"
+
+
+class Client
+  constructor: (@settings) ->
+    @headers =
+      'User-Agent': "Node-Rets/1.0"
+      'RETS-Version': @settings.version || 'RETS/1.7.2'
+      
+    if @settings.userAgent
+      # use specified user agent
+      @headers['User-Agent'] = @settings.userAgent
+
+      # add RETS-UA-Authorization header
+      if @settings.userAgentPassword
+        a1 = crypto.createHash('md5').update([@settings.userAgent, @settings.userAgentPassword].join(":")).digest('hex')
+        retsUaAuth = crypto.createHash('md5').update([a1, "", @settings.sessionId || "", @settings.version || headers['RETS-Version']].join(":")).digest('hex')
+        @headers['RETS-UA-Authorization'] = "Digest " + retsUaAuth
+
+    @baseRetsSession = request.defaults
+      jar: true
+      headers: @headers
+
+      
+  login: () ->
+    options =
+      uri: @settings.loginUrl
+    if @settings.username && @settings.password
+      options.auth =
+        'user': @settings.username
+        'pass': @settings.password
+        'sendImmediately': false
+    Promise.fromNode (callback) =>
+      auth.login @baseRetsSession.defaults(options), callback
+    .then (systemData) =>
+      @systemData = systemData
+      @urls = {}
+      for key,val in URL_KEYS
+        if @systemData[URL_KEYS.GET_METADATA]
+          @urls[URL_KEYS.GET_METADATA] = appUtils.getValidUrl(@systemData[URL_KEYS.GET_METADATA], @settings.loginUrl)
+      @retsVersion = @systemData[OTHER_KEYS.RETS_VERSION]
+      @retsServer = @systemData[OTHER_KEYS.RETS_SERVER]
+      @memberName = @systemData[OTHER_KEYS.MEMBER_NAME]
+      @user = @systemData[OTHER_KEYS.USER]
+      @broker = @systemData[OTHER_KEYS.BROKER]
+      @metadataVersion = @systemData[OTHER_KEYS.METADATA_VERSION]
+      @metadataTimestamp = @systemData[OTHER_KEYS.METADATA_TIMESTAMP]
+      @minMetadataTimestamp = @systemData[OTHER_KEYS.MIN_METADATA_TIMESTAMP]
+
+      metadataModule = metadata @baseRetsSession.defaults uri: @systemData[URL_KEYS.GET_METADATA]
+      @metadata = {}
+      for own method of metadataModule
+        @metadata[method] = Promise.promisify metadataModule[method], metadataModule
+  
+      searchModule = search @baseRetsSession.defaults uri: @systemData[URL_KEYS.SEARCH]
+      @search = {}
+      for own method of searchModule
+        @search[method] = Promise.promisify searchModule[method], searchModule
+  
+      objectsModule = object @baseRetsSession.defaults uri: @systemData[URL_KEYS.GET_OBJECT]
+      @objects = {}
+      for own method of objectsModule
+        @objects[method] = Promise.promisify objectsModule[method], objectsModule
+
+      @logoutRequest = @baseRetsSession.defaults uri: @systemData[URL_KEYS.LOGOUT]
+      
+      return @
+
+  # Logs the user out of the current session
+  logout: () ->
+    Promise.fromNode (callback) =>
+      auth.logout(@logoutRequest, callback)
+
+module.exports = Client
--- test/simple-integration.js
+++ test/simple-integration.js
@@ -1,38 +1,7 @@
-rets-promise
-===========
-A RETS (Real Estate Transaction Standard) client for Node.js with a clean, promise-based interface.
+var fs = require('fs');
+var rets = require('../index');
 
-Forked originally from [rets-client](https://github.com/sbruno81/rets-client), with the user-facing API rewritten
-with breaking changes to use a Promise-based interface.  Promises in this module are provided by
-[Bluebird](https://github.com/petkaantonov/bluebird).  This library contains some CoffeeScript, but may be used in any
-Node app, whether CoffeeScript is in use or not.
-
-The original module was developed against a server running RETS v1.7.2, so there may be incompatibilities with other
-versions.  However, the developers at RealtyMaps want this library to work against any RETS server versions that are
-in current use, so issue tickets describing the problems or (even better) pull requests that fix interactions
-with other RETS versions are welcomed.
-
-[RETS Specifications](http://www.reso.org/specifications)
-
-## Contributions
-Issue tickets and pull requests are welcome.  Unless a breaking refactor is necessary, the `auth`, `metadata`,
-`multipart`, `object`, `search`, and `utils` files in lib/ should be kept JavaScript in order to facilitate PRs
-to/from other `rets-client` forks.
-
-Ideally, pull requests should include tests and match existing code style.
-
-#### TODO
-- Update dependency versions
-- move off https://github.com/Gozala/crypto to use node-native instead
-- add support for UA password as described here: https://github.com/sbruno81/rets-client/issues/1
--- might already be handled
-- make sure listeners are deregistering so app will exit after logout
-
-
-## Example RETS Session
-```javascript
-  var rets = require('rets-promise');
-  // establish connection to RETS server which auto-logs out when we're done
+module.exports = function(settings, photoId) {
   rets.getAutoLogoutClient(settings, function (client) {
     //get resources metadata
     return client.metadata.getResources()
@@ -81,20 +50,19 @@ Ideally, pull requests should include tests and match existing code style.
         return data.Fields
       }).then(function (fields) {
         //perform a query using DQML -- pass resource, class, and query
-        client.search.query("OpenHouse", "OPENHOUSE", "(OpenHouseType=PUBLIC),(ActiveYN=1)")
-          .then(function (results) {
-            console.log("======== OpenHouse Query Results ========");
-            //iterate through search results
-            for (var dataItem = 0; dataItem < results.length; dataItem++) {
-              console.log("-------- Open House "+dataItem+" --------");
-              for (var fieldItem = 0; fieldItem < fields.length; fieldItem++) {
-                var systemStr = fields[fieldItem].SystemName;
-                console.log(systemStr + " : " + results[dataItem][systemStr]);
-              }
-
-              console.log("\n");
+        return client.search.query("OpenHouse", "OPENHOUSE", "(OpenHouseType=PUBLIC),(ActiveYN=1)")
+        .then(function (results) {
+          console.log("======== OpenHouse Query Results ========");
+          //iterate through search results
+          for (var dataItem = 0; dataItem < results.length; dataItem++) {
+            console.log("-------- Open House " + dataItem + " --------");
+            for (var fieldItem = 0; fieldItem < fields.length; fieldItem++) {
+              var systemStr = fields[fieldItem].SystemName;
+              console.log(systemStr + " : " + results[dataItem][systemStr]);
             }
-          });
+            console.log("");
+          }
+        });
       }).then(function () {
         // get photos
         return client.objects.getPhotos("Property", "LargePhoto", photoId)
@@ -112,4 +80,4 @@ Ideally, pull requests should include tests and match existing code style.
   }).catch(function (error) {
     console.log("ERROR: issue encountered: "+(error.stack||error));
   });
-```
+};
